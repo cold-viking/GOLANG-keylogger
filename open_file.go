dbfc1bff2dc53901aa51f466e9dc540ee8c109cf
cold-viking <vovashagapov@gmail.com>
2026-04-24 00:31:40 +0500

    refactor: change func to create/open log file

--- open_file.go
+++ open_file.go
@@ -7,7 +7,7 @@ import (
 
 func openLogFile() (*os.File, error) {
 	nameLogFile := "log_" + time.Now().Format("2006-01-02_15-04-05") + ".txt"
-	file, err := os.Create(nameLogFile)
+	file, err := os.OpenFile(nameLogFile, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		return nil, err
 	}
